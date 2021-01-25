1491c3b2393153979c0cfb08132311e6597ed407
Aleksey-Bur <Bearaleks@mail.ru>
2021-01-25 11:56:39 +0300

    Money Transfer

---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int remittance = 500_000_000;
+        int rest = balance + remittance;
+        System.out.print(rest);
+    }
+}
